19fe045dd29dd12c212de1484dd77f729099aa79
Nikita_MSI_PX60 <mnl88@yandex.ru>
2020-12-29 22:28:12 +0300

    123ewq

---
+++ data/config.py
@@ -0,0 +1,36 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("API_TOKEN_MAIN")
+
+admins = [
+    os.getenv("Nikita_ID"),
+]
+
+ip = os.getenv("ip")
+
+
+COD_CHAT_ID = -1001152933021
+
+# DATABASE
+driver_name = 'postgres'
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+# PSN
+PSN_EMAIL = os.getenv('PSN_EMAIL')
+PSN_PASSWORD = os.getenv('PSN_PASSWORD')
+PSN_USERNAME = os.getenv('PSN_USERNAME')
+
+
+if __name__ == '__main__':
+    print(admins)
+    print(ip)
+    print(BOT_TOKEN)
+    print(PSN_EMAIL)
